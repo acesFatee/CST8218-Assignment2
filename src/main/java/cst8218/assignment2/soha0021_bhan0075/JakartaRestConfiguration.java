761d40933d4452ac45da69c332336c2b68a63ee7
Harkaran Sohal <soha0021@algonquinlive.com>
2025-03-25 17:38:22 -0400

    Initial Commit

---
+++ src/main/java/cst8218/assignment2/soha0021_bhan0075/JakartaRestConfiguration.java
@@ -0,0 +1,13 @@
+package cst8218.assignment2.soha0021_bhan0075;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+/**
+ * Configures Jakarta RESTful Web Services for the application.
+ * @author Juneau
+ */
+@ApplicationPath("resources")
+public class JakartaRestConfiguration extends Application {
+    
+}
